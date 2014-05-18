1fe11711accc3c99a07be8b3c11e5e1c46a9cb1a
hgw <j2160204@gmail.com>
2014-05-18 23:09:01 +0800

    有錯誤需再檢查

--- js/controller.js
+++ js/controller.js
@@ -67,13 +67,6 @@
 						Math.pow( Math.abs( p1.y-p2.y ), 2 ), 
 						0.5 ) /speed;
 			};
-			var getJTAry = function ( path, speed ) {
-				var tl = [];
-				for ( var p = 0, c = 1 ; c < path.length ; p = c, c++ )
-					tl.push( getJT( path[p], path[c], speed ) );
-				
-				return tl;
-			};
 
 			var insertPoint = function ( path, pp ) {
 				for ( var i = 0 ; i < path.length ;  i++ ) {
@@ -85,24 +78,83 @@
 
 				path.push( pp );
 				return path;
-			}
+			};
 
 			var getPossiblePath = function( oPath, param ) {
-				// 合理編排路徑
+				var outPath = oPath.slice( 1, oPath.length-1 ); 
+				// 排除場站點
 				var pathList = param.Client.List.map( function ( rec ) {
-					var tmp = oPath.slice( 1,oPath.length-1 ); // 排除場站點
+					var tmp = angular.copy( outPath );
 					insertPoint( tmp, {rp:rec.o} );
 					insertPoint( tmp, {rp:rec.d} );
+					tmp.client = rec;
 					return tmp;
 				} );
-				// 前後插入場站
+				// 依照時間先後編排路徑
 				pathList.forEach( function ( rec ) {
 					rec.splice( 0, 0, { rp: angular.copy( param.Site ) } );
 					rec.push( { rp: angular.copy( param.Site ) } );
 				} );
+				// 前後插入場站
+				pathList.forEach( function ( path ) {
+					path.forEach( function ( rec ) { rec.lt = rec.at = 0; } );
+					path.reduce( function ( pre, cur, index ) {
+						var jt = getJT( pre.rp.p, cur.rp.p, param.Car.Speed );
+						if ( index == 1 )
+							pre.lt = ( jt < ( cur.rp.t-param.Client.WS ) )? jt : ( ( cur.rp.t-param.Client.WS )-jt );
+						// 剛出場站，調整場站離開時間。提早：離開時間設定為剛好於時間窗開啟到達；晚到：假設落於時間窗內。
+
+						cur.at = pre.lt+jt;
+						cur.lt = param.Car.S+( cur.at < ( cur.rp.t-param.Client.WS ) ) ? ( cur.rp.t-param.Client.WS ) : cur.at ;
+						return cur;
+					} );
+				} );
 				// 計算各點抵達即離開時間
-				// 限制式篩選
-				return[];
+				var condition = [
+					function ( path, param ) { // 車容量限制
+						var stackLog = path.reduce( function ( log, c, idx ) {
+							if ( /^.*\+$/.test(c.rp.name) )
+								log.push(log[idx-1]+1);
+							else if ( /^.*\-$/.test(c.rp.name) )
+								log.push(log[idx-1]-1);
+							else
+								log.push(0);
+
+							return log;
+						}, [] );
+						var result = 0;
+						for ( var i = 0 ; i < stackLog.length ; i++ )
+							if ( stackLog[i] > result )
+								result = stackLog[i];
+
+						return param.Car.C >= result;
+					},
+					function ( path, param ) { // 時間窗限制
+						return path.slice( 1, path.length-1 ).every( function ( rec ) { return rec.at < rec.rp.t+param.Client.WS; } );
+					},
+					function ( path, param ) { // 乘客搭乘時間限制
+						var cl = path.slice( 1, path.length-1 ).sort( function ( a, b ) {
+							return Number( a.rp.name.match(/\d+/)[0] )-Number( b.rp.name.match(/\d+/)[0] );
+						} );
+						for ( var i = 0 ; i < cl.length ; i+=2 )
+							if ( param.Client.R < ( cl[i+1].at-cl[i].lt ) )
+								return false;
+						
+						return true;
+					},
+					function ( path, param ) { // 駕駛時間限制
+						return param.Car.WT >= ( path[path.length-1].at - path[0].lt );
+					}
+				];
+				var result = pathList.filter( function ( path, idx ) {
+					return condition.every( function ( confunct, cidx ) {
+						var checkValue = confunct( path, param ); 
+						console.log( checkValue, idx, cidx );
+						return checkValue;
+					} );
+				} );
+				// 篩選符合所有限制式的路徑
+				return result;
 			};
 
 			$scope.execute = function () {
@@ -116,14 +168,26 @@
 					} );
 				// 依照開始時間窗先後順序對客戶名單進行排序
 				// 標記客戶需求點
-				console.log( param.Client.List );
+				// console.log( param.Client.List );
 				var CarPathLog = [];
-				for ( var k = 1 ; k <= param.Car.K ; k++ ) {
+				for ( var k = 0 ; k < param.Car.K ; k++ ) {
 					CarPathLog[k] = [ { rp : angular.copy( param.Site ) } ];
 					/* { at lt rp } */
-					var pathList = undefined ; 
-					while ( ( pl = getPossiblePath( CarPathLog[k].path, param ) ).length != 0 ) { // 找出可行路徑
+					
+					for ( var ppList = undefined ; ( ppList = getPossiblePath( CarPathLog[k], param ) ).length != 0 ; ) { 
+						// 判斷有無可行路徑
+						var chosePath = ppList.sort( function ( aPath, bPath ) {
+							var aInaccuracy = aPath.slice( 1, aPath.length-1 ).reduce( function ( value, rec ) {
+								value+=Math.abs(rec.at-rec.rp.t);
+							}, 0 );
+							var bInaccuracy = bPath.slice( 1, bPath.length-1 ).reduce( function ( value, rec ) {
+								value+=Math.abs(rec.at-rec.rp.t);
+							}, 0 );
+							return aInaccuracy-bInaccuracy;
+						} )[0];
 						// 決定路徑
+						param.Client.List.splice( param.Client.List.indexOf(chosePath.client), 1 );
+						CarPathLog[k] = chosePath;
 						// 改變路徑
 					}
 				}
